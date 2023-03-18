7cae54b85217d89a92137cd67aff7eadef6a3791
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-03-18 08:50:50 +0530

    Updated FeedbackList.jsx file

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import FeedbackItem from './FeedbackItem';
 
 function FeedbackList(props) {
+  if(!props.feedback || props.feedback.length === 0) {
+    return <p>No feedbacks yet!!</p>
+  }
   return (
     <div className='feedback-list'>
       {props.feedback.map((item) => <FeedbackItem key={item} item={item} handleDelete={props.handleDelete}/>)}
